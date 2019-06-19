14605cbd6450626d3c2930559a62372899eaba94
Muthomi <muthomimate@gmail.com>
2019-06-19 18:53:29 +0300

    feat(loginregister): add login and register components

---
+++ src/common/components/Login.js
@@ -0,0 +1,85 @@
+import React, { Component } from "react";
+import { View, TextInput, StyleSheet, Button, TouchableOpacity, Text } from "react-native"
+import Register from "./Register";
+// import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view"
+
+class Login extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    render() {
+        const { style } = this.props;
+        return (
+            <View
+            style={style}
+            >
+                <View style={styles.inputs}>
+                    <View style={styles.email}>
+                        <TextInput
+                            style={{textAlign: "center", height: "100%"}}
+                            placeholder="Email"
+                            keyboardType="email-address"
+                        />
+                    </View>
+                    <View style={styles.password}>
+                        <TextInput
+                            placeholder="Password"
+                            style={{textAlign: "center", height: "100%"}}
+                        />
+                    </View>
+                </View>
+                <View style={styles.buttons}>
+                    <Button
+                        title="Register"
+                        color="#FFFFFF"
+                    />
+                    <TouchableOpacity style={styles.touchable}>
+                        <Text style={{color: "#FFFFFF"}}> Forgot Password?</Text>
+                    </TouchableOpacity>
+                    <Button
+                        title="Login"
+                        color="#FFFFFF"
+                    />
+                </View>
+            </View>
+        )
+    }
+}
+const styles = StyleSheet.create({
+    inputs: {
+        flex: 1,
+        width: "100%",
+        height: "80%",
+        borderRadius: 10,
+        flexDirection: "column",
+        backgroundColor: "#FFFFFF"
+    },
+    email: {
+        borderBottomColor: "black",
+        borderBottomWidth: 1,
+        height: "50%"
+    },
+    password: {
+        borderTopColor: "black",
+        borderTopWidth: 1,
+        height: "50%"
+    },
+    buttons: {
+        flex: 1,
+        fontSize: 8,
+        width: "100%",
+        height:"20%",
+        flexDirection: "row"
+    },
+    touchable: {
+        marginTop: "5%",
+
+    }
+
+})
+export default Login
---
+++ src/common/components/Register.js
@@ -0,0 +1,184 @@
+import React, { Component } from "react";
+import { View, Text, Image, StyleSheet, TextInput, KeyboardAvoidingView, Platform, TouchableOpacity } from "react-native";
+import { Icon, Button } from "react-native-elements";
+
+class Register extends Component {
+    constructor(props){
+        super(props)
+        this.state= {
+            password: "",
+            confirmPassword: "",
+            showPassword: true
+        }
+    }
+    onChange = (event, input) => {
+        if(input === "password"){
+            this.setState
+        }
+    }
+
+    render(){
+        const {password, confirmPassword, showPassword} =this.state
+        return (
+            <View style={styles.container}>
+                <Image source = {require('../../images/back1.jpg')} style={styles.image}/>
+                <View style={styles.textView}>
+                    { /* <Image source = {require('../../images/car.png')} style={{width: "50%"}}/>*/ }
+                    <Text style={styles.text}>
+                        <Text style={{color: "#C4A751"}}>GRAB</Text>
+                        <Text style={{color: "#FFFFFF"}}>CAB</Text>
+                    </Text>
+                </View>
+                <KeyboardAvoidingView style={styles.registerView}>
+                    <View style={styles.text}>
+                        <Text style={{color: "#FFFFFF"}}>Registration</Text>
+                    </View>
+                    <View style={styles.input}>
+                        <View style={styles.icon}>
+                            <Icon name="user" size={25} type="font-awesome" color="#FFFFFF"/>
+                        </View>
+                        <View style={styles.border}>
+                            <TextInput
+                                style={{ height: "100%", color:"#FFFFFF"}}
+                                placeholder="First Name"
+                                placeholderTextColor="#FFFFFF"
+                            />
+                        </View>
+                    </View>
+                    <View style={styles.input}>
+                        <View style={styles.icon}>
+                            <Icon name="user" size={25} type="font-awesome" color="#FFFFFF"  />
+                        </View>
+                        <View style={styles.border}>
+                            <TextInput
+                                style={{ height: "100%", color:"#FFFFFF"}}
+                                placeholder="Last Name"
+                                placeholderTextColor="#FFFFFF"
+                            />
+                        </View>
+                    </View>
+                    <View style={styles.input}>
+                        <View style={styles.icon}>
+                            <Icon name="envelope-o" size={25} type="font-awesome" color="#FFFFFF" />
+                        </View>
+                        <View style={styles.border}>
+                            <TextInput
+                                style={{ height: "100%", color:"#FFFFFF"}}
+                                placeholder="Email"
+                                keyboardType="email-address"
+                                placeholderTextColor="#FFFFFF"
+                            />
+                        </View>
+                    </View>
+                    <View style={styles.input}>
+                        <View style={styles.icon}>
+                            <Icon name="mobile" size={25} type="font-awesome" color="#FFFFFF" />
+                        </View>
+                        <View style={styles.border}>
+                            <TextInput
+                                style={{ height: "100%", color:"#FFFFFF"}}
+                                placeholder="Mobile"
+                                keyboardType="phone-pad"
+                                placeholderTextColor="#FFFFFF"
+                            />
+                        </View>
+                    </View>
+                    <View style={styles.input}>
+                        <View style={styles.icon}>
+                            <Icon name="lock" size={25} type="font-awesome" color="#FFFFFF" />
+                        </View>
+                        <View style={styles.border}>
+                            <TextInput
+                                style={{ height: "100%", color:"#FFFFFF"}}
+                                value={password}
+                                onChangeText={(text) => this.setState({password: text})}
+                                placeholder="Password"
+                                secureTextEntry={showPassword}
+                                placeholderTextColor="#FFFFFF"
+                            />
+                        </View>
+                    </View>
+                    <View style={styles.input}>
+                        <View style={styles.icon}>
+                            <Icon name="lock" size={25} type="font-awesome" color="#FFFFFF" />
+                        </View>
+                        <View style={styles.border}>
+                            <TextInput
+                                style={{ height: "100%", color:"#FFFFFF"}}
+                                placeholder="Confirm password"
+                                onChangeText={(text) => this.setState({confirmPassword: text})}
+                                value={confirmPassword}
+                                secureTextEntry={showPassword}
+                                placeholderTextColor="#FFFFFF"
+                            />
+                        </View>
+                    </View>
+                    <TouchableOpacity style={styles.buttonView}>
+                        <Text style={{color: "#FFFFFF", textAlign: "center"}}> Register Now</Text>
+                    </TouchableOpacity>
+                </KeyboardAvoidingView>
+
+            </View>
+        )
+    }
+}
+const styles = StyleSheet.create({
+    container: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+      },
+      image: {
+        width: "100%",
+        height: "100%",
+        flex:1,
+    },
+    registerView: {
+        position: "absolute",
+        bottom: "5%",
+        width: "70%",
+        left: "15%",
+        alignItems: "center",
+        textAlign: "center"
+    },
+    border: {
+        borderBottomColor: "white",
+        borderBottomWidth: 1,
+        width: "85%"
+    },
+    input: {
+        flex: 1,
+        flexDirection: "row",
+        margin: 10,
+        width:"100%",
+    },
+    icon: {
+        width: "10%",
+        marginRight: 10
+    },
+    buttonView: {
+        width:"60%",
+        flex: 1,
+        height: 40,
+        marginTop: "5%",
+        textAlign: "center",
+        backgroundColor: "#1F81D4",
+        borderRadius: 10,
+        justifyContent: "space-around"
+    },
+    text: {
+        fontSize: 40,
+        fontWeight: "bold",
+        fontFamily: 'Cochin',
+    },
+    textView: {
+        position: "absolute",
+        top: "20%",
+        width: "100%",
+        alignItems: "center",
+        textAlign: "center",
+    }
+});
+export default Register;
